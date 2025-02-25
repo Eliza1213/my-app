99936a1189be4dfe49371c7e0417613de1379d3c
Eliza1213 <20230015@uthh.edu.mx>
2025-02-25 14:47:20 -0600

    25/02/2025

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './app/Componetes/Header';
 import Foother from './app/Componetes/Foother';
-import Calculadora from './app/screens/Calculadora';
+import Calculadora from './app/screens';
 
 export default function App() {
   return (
